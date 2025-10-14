9d97dd5960a870343ad5a725b5f7f72eb715b727
FeralAlgae <cazocarma@gmail.com>
2025-10-14 15:05:03 -0300

    tarea: Se crearon los endpoints, los modelos y controladores de la tarea, se adicionó un NGINX para resolver las peticiones http

---
+++ resources/js/components/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="text-center mt-5">
+    <h1>Laravel + Vue 3 ToDo App</h1>
+    <p>¡Integración lista!</p>
+  </div>
+</template>
+
+<script setup>
+</script>
+
+<style scoped>
+h1 {
+  color: #2c3e50;
+}
+</style>
